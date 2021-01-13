66925db07942c619194b8e916323a41c45928fd6
John_Vera <johnalexiscristobaljimenez6@gmail.com>
2021-01-12 22:35:03 -0600

    FragmentContactr

--- app/src/main/java/com/bicisos/i7/bicisos/ui/dashboard/contact/ContactFragmentGtt.kt
+++ app/src/main/java/com/bicisos/i7/bicisos/ui/dashboard/contact/ContactFragmentGtt.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.bicisos.i7.bicisos.R
+import kotlinx.android.synthetic.main.fragment_contact_gtt.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,12 +31,28 @@ class ContactFragmentGtt : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_contact_gtt, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        textView44.setOnClickListener {
+
+        }
+
+        textView45.setOnClickListener {
+
+        }
+
+        imageView10.setOnClickListener {
+
+        }
+
+        buttonEnviarContact.setOnClickListener {
+            val text = editTextContactoText.text.toString()
+        }
     }
 
     companion object {
